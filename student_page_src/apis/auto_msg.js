0f28f53613850295629ec3f3e87ada2efbf6c29c
XXT <ku8834367@163.com>
2019-05-20 11:57:21 +0800

    修复生成环境学生问答页面api地址

--- student_page_src/apis/auto_msg.js
+++ student_page_src/apis/auto_msg.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 const is_dev = process.env.NODE_ENV === 'development'
 
-const BASE_URL = is_dev? 'http://bs.dev.local': 'http://gptp.dev.xuxiantao.xin'
-
+const BASE_URL = is_dev? 'http://bs.dev.local': '/qa/api'
 const getAnswer = (msg) => {
   const url = `${BASE_URL}/v1/nlp/getanswer`
   const params = {
